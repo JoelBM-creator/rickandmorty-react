8025fb8b964391394c02e93673a755984b7c552e
Joel Berenguer Martinez <joel7397@linkiafp.es>
2020-06-10 14:05:31 +0200

    Updates

---
+++ precache-manifest.793ec1d9864ee610d5e8337bd28790ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb655706b09e89307b97a5c0bd66679",
+    "url": "/rickandmorty-react/index.html"
+  },
+  {
+    "revision": "4d0afee07ed8e4919136",
+    "url": "/rickandmorty-react/static/js/2.95bfcae5.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/rickandmorty-react/static/js/2.95bfcae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f4ae873ba28883fa76",
+    "url": "/rickandmorty-react/static/js/main.c06e868e.chunk.js"
+  },
+  {
+    "revision": "12e183acce9305361427",
+    "url": "/rickandmorty-react/static/js/runtime-main.dba0b412.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "/rickandmorty-react/static/media/logo.75509f2e.png"
+  }
+]);
